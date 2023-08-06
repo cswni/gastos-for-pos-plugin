2a3d88684d299a14bc4d0ef56110abd7de45590c
cswni <cswni@users.noreply.github.com>
2023-08-06 20:31:45 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,11 @@
 
 namespace Cswni\GastosForPosPlugin\Tests;
 
+use Cswni\GastosForPosPlugin\GastosForPosPluginServiceProvider;
 use Filament\FilamentServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Cswni\GastosForPosPlugin\GastosForPosPluginServiceProvider;
 
 class TestCase extends Orchestra
 {
